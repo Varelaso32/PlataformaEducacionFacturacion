043759acd0ce6289b7bec10a43483b5df12e95fb
Varelaso32 <samyst2013@hotmail.com>
2025-03-06 18:10:19 -0500

    ✨ feat(Change func calcular):
    Se cambia la funcion calcularCostoCursosAdicionales para que funcione mejor y de un recorrido mejor.

--- Php/classCurso.php
+++ Php/classCurso.php
@@ -1,67 +1,72 @@
-<?php
+    <?php
 
-class Curso 
-{
-    private $nombre;
-    private $costo_base;
-
-    public function __construct($nombre)
+    class Curso
     {
-        $this->nombre = $nombre;
-        $this->costo_base = $this->calcularCostoBase();
-    }
+        private $nombre;
+        private $costo_base;
 
-    private function calcularCostoBase()
-    {
-        switch ($this->nombre) {
-            case 'Programación':
-                return 600000;
-            case 'Diseño Gráfico':
-                return 500000;
-            case 'Marketing Digital':
-                return 450000;
-            case 'Finanzas':
-                return 550000;
-            case 'Ciberseguridad':
-                return 700000;
-            default:
-                return 0;
+        public function __construct($nombre)
+        {
+            $this->nombre = $nombre;
+            $this->costo_base = $this->calcularCostoBase();
         }
-    }
 
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
-
-    public function getCostoBase()
-    {
-        return $this->costo_base;
-    }
-
-    public function getCursosAdicionalesDisponibles()
-    {
-        return [
-            "Inteligencia Artificial" => 500000,
-            "UX/UI" => 350000,
-            "Redes Sociales" => 300000,
-            "Análisis de Datos" => 450000,
-            "Gestión de Proyectos" => 400000
-        ];
-    }
-
-    public function calcularCostoCursosAdicionales($cursosSeleccionados)
-    {
-        $cursosDisponibles = $this->getCursosAdicionalesDisponibles();
-        $total = 0;
-
-        foreach ($cursosSeleccionados as $curso) {
-            if (isset($cursosDisponibles[$curso])) {
-                $total += $cursosDisponibles[$curso];
+        private function calcularCostoBase()
+        {
+            switch ($this->nombre) {
+                case 'Programación':
+                    return 600000;
+                case 'Diseño Gráfico':
+                    return 500000;
+                case 'Marketing Digital':
+                    return 450000;
+                case 'Finanzas':
+                    return 550000;
+                case 'Ciberseguridad':
+                    return 700000;
+                default:
+                    return 0;
             }
         }
 
-        return $total;
+        public function getNombre()
+        {
+            return $this->nombre;
+        }
+
+        public function getCostoBase()
+        {
+            return $this->costo_base;
+        }
+
+        public function getCursosAdicionalesDisponibles()
+        {
+            return [
+                "Inteligencia Artificial" => 500000,
+                "UX/UI" => 350000,
+                "Redes Sociales" => 300000,
+                "Análisis de Datos" => 450000,
+                "Gestión de Proyectos" => 400000
+            ];
+        }
+
+        public function calcularCostoCursosAdicionales($cursosSeleccionados)
+        {
+            $cursosDisponibles = $this->getCursosAdicionalesDisponibles();
+            $total = 0;
+
+            $nombresCursos = array_keys($cursosDisponibles);
+
+            for ($i = 0; $i < count($cursosSeleccionados); $i++) {
+                
+                for ($j = 0; $j < count($nombresCursos); $j++) {
+                    if ($cursosSeleccionados[$i] == $nombresCursos[$j]) {
+                        $total += $cursosDisponibles[$nombresCursos[$j]];
+                    }
+                }
+            }
+
+            return $total;
+        }
     }
-}
-?>
+    ?>
